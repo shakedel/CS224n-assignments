ef0adaa02ddff3632532e0895e2d6a9d7ae46cff
Elad Shaked <ELADSH@il.ibm.com>
2018-02-15 09:38:04 +0200

    clean assignment1 project

---
+++ assignment1-sol/q2_sigmoid.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import numpy as np
+from numbers import Number
+import math
+
+def sigmoid(x):
+    """
+    Compute the sigmoid function for the input here.
+
+    Arguments:
+    x -- A scalar or numpy array.
+
+    Return:
+    s -- sigmoid(x)
+    """
+
+    ### YOUR CODE HERE
+    return 1 / (1 + np.exp(-1 * x))
+    ### END YOUR CODE
+
+
+def sigmoid_grad(s):
+    """
+    Compute the gradient for the sigmoid function here. Note that
+    for this implementation, the input s should be the sigmoid
+    function value of your original input x.
+
+    Arguments:
+    s -- A scalar or numpy array.
+
+    Return:
+    ds -- Your computed gradient.
+    """
+
+    ### YOUR CODE HERE
+    return s - np.power(s, 2)
+    ### END YOUR CODE
+
+def test_sigmoid_basic():
+    """
+    Some simple tests to get you started.
+    Warning: these are not exhaustive.
+    """
+    print "Running basic tests..."
+    x = np.array([[1, 2], [-1, -2]])
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = np.array([
+        [0.73105858, 0.88079708],
+        [0.26894142, 0.11920292]])
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = np.array([
+        [0.19661193, 0.10499359],
+        [0.19661193, 0.10499359]])
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    print "You should verify these results by hand!\n"
+
+
+def test_sigmoid():
+    """
+    Use this space to test your sigmoid implementation by running:
+        python q2_sigmoid.py
+    This function will not be called by the autograder, nor will
+    your tests be graded.
+    """
+    x = 2
+    f = sigmoid(x)
+    g = sigmoid_grad(f)
+    print f
+    f_ans = 0.88079708
+    assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
+    print g
+    g_ans = 0.10499359
+    assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
+    ### END YOUR CODE
+
+
+if __name__ == "__main__":
+    test_sigmoid_basic()
+    test_sigmoid()
